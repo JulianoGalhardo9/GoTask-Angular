c16af9cad3e49a255b227d943b56e3827d64e551
Juliano Galhardo de Oliveira <julianogalhardo@192.168.0.12>
2026-03-24 22:44:06 -0300

    feat: Creating the method to add a task

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { ITask } from "../Interfaces/task.interface";
+import { ITaskFormControls } from "../Interfaces/task-form-controls.interface";
+import { TaskStatusEnum } from "../enums/task.status.enum";
+import { generateUniqueIdWithTimestamp } from "../utils/generate-unique-id-with-timestamp";
 
 @Injectable({
     providedIn: "root",
@@ -18,4 +21,15 @@ export class TaskService {
     // Tarefas em Concluído
     private doneTasks$ = new BehaviorSubject<ITask[]>([]);
     readonly doneTasks = this.doneTasks$.asObservable();
+
+    addTask(taskInfos: ITaskFormControls) {
+        const newTask: ITask = {
+            ...taskInfos,
+            status: TaskStatusEnum.TODO,
+            id: generateUniqueIdWithTimestamp(),
+            comments: [],
+        };
+        const currentTasks = this.todoTasks$.value;
+        this.todoTasks$.next([...currentTasks, newTask]);
+    }
 }
