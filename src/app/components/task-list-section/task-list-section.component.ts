efb878b91b20d3b48621a29d603f1824a36ef080
Juliano Galhardo de Oliveira <julianogalhardo@192.168.0.12>
2026-03-25 16:13:46 -0300

    feat: Updating the status of the moved task in the component.

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -3,8 +3,9 @@ import { TaskCardComponent } from '../task-card/task-card.component';
 import { TaskService } from '../../Services/task.service';
 import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ITask } from '../../Interfaces/task.interface';
-import { A } from '@angular/cdk/keycodes';
 import { AsyncPipe } from '@angular/common';
+import { TaskStatus } from '../../types/task.status';
+import { TaskStatusEnum } from '../../enums/task.status.enum';
 
 @Component({
   selector: 'app-task-list-section',
@@ -15,9 +16,40 @@ import { AsyncPipe } from '@angular/common';
 export class TaskListSectionComponent {
 
   public readonly _taskService = inject(TaskService);
-todoTasks: unknown;
 
-  drop(event: CdkDragDrop<ITask[]>) {
+  onCardDrop(event: CdkDragDrop<ITask[]>) {
+    this.moveCardToColumn(event);
+
+    const taskId = event.item.data.id;
+    const taskCurrentStatus = event.item.data.status;
+    const droppedColumn = event.container.id;
+
+    this.updateTaskStatus(taskId, taskCurrentStatus, droppedColumn);
+  }
+
+  private updateTaskStatus(taskId: string, taskCurrentStatus: TaskStatus, droppedColumn: string) {
+    let taskNextStatus: TaskStatus;
+
+    switch (droppedColumn) {
+      case 'todo-column':
+        taskNextStatus = TaskStatusEnum.TODO;
+        break;
+      case 'doing-column':
+        taskNextStatus = TaskStatusEnum.DOING;
+        break;
+      case 'done-column':
+        taskNextStatus = TaskStatusEnum.DONE;
+        break;
+      default:
+        throw new Error('Coluna não identificada');
+    }
+
+    
+  }
+    
+
+  private moveCardToColumn(event: CdkDragDrop<ITask[]>) {
+    console.log(event)
     if(event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);}
       else{
