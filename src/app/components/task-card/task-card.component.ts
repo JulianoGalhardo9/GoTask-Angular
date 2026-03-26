022a3426e880970ddd754904d734f2e37a2683e9
Juliano Galhardo de Oliveira <julianogalhardo@192.168.0.12>
2026-03-25 23:33:30 -0300

    feat: Updating the task name and description

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input } from '@angular/core';
 import { ModalControllerService } from '../../Services/modal-controller.service';
 import { ITask } from '../../Interfaces/task.interface';
+import { TaskService } from '../../Services/task.service';
 
 @Component({
   selector: 'app-task-card',
@@ -11,15 +12,25 @@ import { ITask } from '../../Interfaces/task.interface';
 export class TaskCardComponent {
   @Input({required: true}) task!: ITask;
   private readonly _modalControllerService = inject(ModalControllerService);
+  private readonly _taskService = inject(TaskService);
 
   openEditTaskModal() {
     const dialogRef = this._modalControllerService.openEditTaskModal({
-      name: 'Nome tarefa',
-      description: 'Descrição da tarefa',
+      name: this.task.name,
+      description: this.task.description,
     });
 
     dialogRef.closed.subscribe((taskForm) => {
       console.log('Tarefa editada:', taskForm);
+
+      if(taskForm) {
+        this._taskService.updateTaskNameAndDescription(
+          this.task.id,
+          this.task.status,
+          taskForm.name,
+          taskForm.description,
+        );
+      }
     });
   }
 }
