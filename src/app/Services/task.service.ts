bb0856c37b7355c2fe45002893f44a261e5a3cfa
Juliano Galhardo de Oliveira <julianogalhardo@192.168.0.12>
2026-03-26 21:38:00 -0300

    feat: Saving tasks to LocalStorage

--- src/app/Services/task.service.ts
+++ src/app/Services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, map } from "rxjs";
+import { BehaviorSubject, map, tap } from "rxjs";
 import { ITask } from "../Interfaces/task.interface";
 import { ITaskFormControls } from "../Interfaces/task-form-controls.interface";
 import { TaskStatusEnum } from "../enums/task.status.enum";
@@ -15,17 +15,26 @@ export class TaskService {
     // Tarefas em A fazer
     private todoTasks$ = new BehaviorSubject<ITask[]>([]);
     readonly todoTasks = this.todoTasks$.asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)),);
+    .pipe(
+        map((tasks) => structuredClone(tasks)),
+        tap((tasks) => this.saveTasksOnLocalStorage(TaskStatusEnum.TODO, tasks)),
+    );
 
     // Tarefas em Andamento
     private doingTasks$ = new BehaviorSubject<ITask[]>([]);
     readonly doingTasks = this.doingTasks$.asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)),);;
+    .pipe(
+        map((tasks) => structuredClone(tasks)),
+        tap((tasks) => this.saveTasksOnLocalStorage(TaskStatusEnum.DOING, tasks)),
+    );
 
     // Tarefas em Concluído
     private doneTasks$ = new BehaviorSubject<ITask[]>([]);
     readonly doneTasks = this.doneTasks$.asObservable()
-    .pipe(map((tasks) => structuredClone(tasks)),);;
+    .pipe(
+        map((tasks) => structuredClone(tasks)),
+        tap((tasks) => this.saveTasksOnLocalStorage(TaskStatusEnum.DONE, tasks)),
+    );
 
     addTask(taskInfos: ITaskFormControls) {
         const newTask: ITask = {
@@ -94,12 +103,20 @@ export class TaskService {
     currentTaskList.next(newTaskList);
   }
 
-    private getTaskListByStatus(status: TaskStatus) {
+  private saveTasksOnLocalStorage(key: string, tasks: ITask[]) {
+    try {
+        localStorage.setItem(key, JSON.stringify(tasks));
+    } catch (error) {
+        console.error("Erro ao salvar tarefas no localStorage:", error);
+    }
+  }
+
+  private getTaskListByStatus(status: TaskStatus) {
         const taskListObj = {
             [TaskStatusEnum.TODO]: this.todoTasks$,
             [TaskStatusEnum.DOING]: this.doingTasks$,
             [TaskStatusEnum.DONE]: this.doneTasks$,
         };
         return taskListObj[status];
-    }
+ }
 }
